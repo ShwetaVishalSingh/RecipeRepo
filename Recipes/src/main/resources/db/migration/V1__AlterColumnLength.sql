74e85ddd4cffccfd193bcd26521428bdd79daa86
Vishal Singh <vishal.n.singh@ica.se>
2020-05-17 20:40:32 +0200

    modified length of the column

--- Recipes/src/main/resources/db/migration/V1__AlterColumnLength.sql
+++ Recipes/src/main/resources/db/migration/V1__AlterColumnLength.sql
@@ -1,5 +1,5 @@
-ALTER TABLE public.recipe ALTER COLUMN image TYPE varchar(1100);
-ALTER TABLE public.recipe ALTER COLUMN description TYPE varchar(1100);
-ALTER TABLE public.recipe ALTER COLUMN ingredients TYPE varchar(1100);
-ALTER TABLE public.recipe ALTER COLUMN steps TYPE varchar(1100);
+ALTER TABLE public.recipe ALTER COLUMN image TYPE varchar(2100);
+ALTER TABLE public.recipe ALTER COLUMN description TYPE varchar(2100);
+ALTER TABLE public.recipe ALTER COLUMN ingredients TYPE varchar(2100);
+ALTER TABLE public.recipe ALTER COLUMN steps TYPE varchar(2100);
 commit;
